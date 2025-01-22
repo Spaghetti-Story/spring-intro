5c5b43e9967845873d087dfa1600843637e2f14c
kuma <yeawon6566@gmail.com>
2025-01-22 22:25:11 +0900

    #섹션1 ~ 섹션8: 스프링 입문 스터디 완강

---
+++ src/main/java/eonwy/hello_spring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,10 @@
+package eonwy.hello_spring.repository;
+
+import eonwy.hello_spring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+    Optional<Member> findByName(String name);
+}
